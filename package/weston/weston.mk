b344f4a0965e8628e1d3c4d4b39e21be097edee8
Zan Dobersek <zdobersek@igalia.com>
2014-12-22 09:40:02 -0800

    [weston] Switch to WK4WL Weston. Enable EGL, DRM backend when requested.

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -4,26 +4,31 @@
 #
 ################################################################################
 
-WESTON_VERSION = 1.6.0
-WESTON_SITE = http://wayland.freedesktop.org/releases
-WESTON_SOURCE = weston-$(WESTON_VERSION).tar.xz
+WESTON_VERSION = 920a0331cf5340942a9d39ac3b48ce207f7970b6
+WESTON_SITE = $(call github,zdobersek,weston,$(WESTON_VERSION))
 WESTON_LICENSE = MIT
 WESTON_LICENSE_FILES = COPYING
 
+WESTON_AUTORECONF = YES
+WESTON_INSTALL_STAGING = YES
 WESTON_DEPENDENCIES = host-pkgconf wayland libxkbcommon pixman libpng \
 	jpeg mtdev udev cairo
 
 WESTON_CONF_OPTS = \
 	--with-dtddir=$(STAGING_DIR)/usr/share/wayland \
-	--disable-egl \
 	--disable-simple-egl-clients \
 	--disable-xwayland \
 	--disable-x11-compositor \
-	--disable-drm-compositor \
 	--disable-wayland-compositor \
 	--disable-headless-compositor \
 	--disable-weston-launch \
 	--disable-colord
+
+ifeq ($(BR2_PACKAGE_WESTON_EGL),y)
+WESTON_CONF_OPTS += --enable-egl
+else
+WESTON_CONF_OPTS += --disable-egl
+endif
 
 ifeq ($(BR2_PACKAGE_LIBINPUT),y)
 WESTON_DEPENDENCIES += libinput
@@ -42,6 +47,12 @@ ifeq ($(BR2_PACKAGE_WESTON_FBDEV),y)
 WESTON_CONF_OPTS += --enable-fbdev-compositor
 else
 WESTON_CONF_OPTS += --disable-fbdev-compositor
+endif
+
+ifeq ($(BR2_PACKAGE_WESTON_DRM),y)
+WESTON_CONF_OPTS += --enable-drm-compositor
+else
+WESTON_CONF_OPTS += --disable-drm-compositor
 endif
 
 ifeq ($(BR2_PACKAGE_WESTON_RPI),y)
