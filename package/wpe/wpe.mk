eee0b361d1dd5c7f082bfaae80c234936a23ccee
Zan Dobersek <zdobersek@igalia.com>
2015-01-12 08:49:50 -0800

    [wpe] Build with HTML5 media support enabled.

--- package/wpe/wpe.mk
+++ package/wpe/wpe.mk
@@ -12,11 +12,10 @@ WPE_INSTALL_STAGING = YES
 WPE_DEPENDENCIES = host-flex host-bison host-gperf host-ruby ruby \
        host-pkgconf zlib pcre libgles libegl cairo freetype fontconfig \
        harfbuzz icu libxml2 libxslt sqlite libsoup jpeg webp \
+       gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad \
        wayland weston
-       # gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad \
 
-WPE_CONF_OPTS = -DPORT=WPE -DENABLE_VIDEO=OFF -DENABLE_VIDEO_TRACK=OFF \
- -DCMAKE_BUILD_TYPE=Release \
+WPE_CONF_OPTS = -DPORT=WPE -DCMAKE_BUILD_TYPE=Release \
  -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align" \
  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align"
 
