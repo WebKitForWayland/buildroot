d3830cecb2c01fad3f03d220aab1727bc05269b9
Zan Dobersek <zdobersek@igalia.com>
2015-02-24 14:01:32 -0800

    [nouveau-drm] Bump to the latest version.

--- package/nouveau-drm/nouveau-drm.mk
+++ package/nouveau-drm/nouveau-drm.mk
@@ -4,19 +4,19 @@
 #
 ################################################################################
 
-NOUVEAU_DRM_VERSION = a502527f75b469e09bfd925b15525195cf134561
+NOUVEAU_DRM_VERSION = 77fe2a4c1c7a85ec438894734008d9c2e6f98a33
 NOUVEAU_DRM_SITE = $(call github,Gnurou,nouveau,$(NOUVEAU_DRM_VERSION))
 
 NOUVEAU_DRM_DEPENDENCIES = linux
 
 define NOUVEAU_DRM_BUILD_CMDS
     cd $(@D)/drm
-    $(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D)/drm modules
+    $(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D)/drm/nouveau modules
 endef
 
 define NOUVEAU_DRM_INSTALL_TARGET_CMDS
     cd $(@D)/drm
-    $(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D)/drm INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
+    $(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D)/drm/nouveau INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
 endef
 
 $(eval $(generic-package))
